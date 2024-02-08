9c125e78e326a5ee81d40210093588eb8ac8e9f0
ACernei <andreicernei9@gmail.com>
2024-02-08 20:03:05 +0200

    Add player

---
+++ scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+@export var speed = 300
+@export var gravity = 30 
+@export var jump_force = 300
+
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += gravity
+		
+	if Input.is_action_just_pressed("jump"): # && is_on_floor():
+		velocity.y = -jump_force
+		
+	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	velocity.x = speed * horizontal_direction
+	
+	if horizontal_direction != 0:
+		switch_direction(horizontal_direction)
+	
+	move_and_slide()
+	
+	update_animation(horizontal_direction)
+
+func update_animation(horizontal_direction):
+	if is_on_floor():
+		if horizontal_direction == 0:
+			ap.play("idle")
+		else:
+			ap.play("run")
+	else:
+		if velocity.y < 0:
+			ap.play("jump")
+		elif velocity.y > 0:
+			ap.play("fall")
+
+
+func switch_direction(horizontal_direction):
+	sprite.flip_h = (horizontal_direction == -1)
+	sprite.position.x = horizontal_direction
